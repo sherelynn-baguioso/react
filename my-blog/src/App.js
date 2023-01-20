6b9d8e5223d01373e90d5fbb62ea1ec0684d8076
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-01-20 13:42:31 +1300

    Create routes for each page

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,4 +1,5 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
 import ArticlesListPage from './pages/ArticlesListPage'
@@ -6,15 +7,19 @@ import ArticlePage from './pages/ArticlePage'
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
       <h1>My Awesome Blog</h1>
       <div id="page-body">
-        <HomePage />
-        <AboutPage />
-        <ArticlesListPage />
-        <ArticlePage />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/articles" element={<ArticlesListPage />} />
+          <Route path="/articles/:articleId" element={<ArticlePage />} />
+        </Routes>
       </div>
     </div>
+    </BrowserRouter>
   )
 }
 
