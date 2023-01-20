296fad4fdcaa51a74b228699407bd49b58457a1a
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-01-20 13:37:22 +1300

    Importing and displaying pages to home page

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,10 +1,19 @@
 import './App.css'
+import HomePage from './pages/HomePage'
+import AboutPage from './pages/AboutPage'
+import ArticlesListPage from './pages/ArticlesListPage'
+import ArticlePage from './pages/ArticlePage'
 
 function App() {
   return (
     <div className="App">
       <h1>My Awesome Blog</h1>
-      <div id="page-body">Welcome to my blog!</div>
+      <div id="page-body">
+        <HomePage />
+        <AboutPage />
+        <ArticlesListPage />
+        <ArticlePage />
+      </div>
     </div>
   )
 }
