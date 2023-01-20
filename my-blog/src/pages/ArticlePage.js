d5b028a5706cbe8b5af3c5aeade95476ad4a43d0
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-01-20 14:00:55 +1300

    Import article content to article page with url paramters

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,5 +1,18 @@
+import { useParams } from 'react-router-dom'
+import articles from './article-content'
+
 const ArticlePage = () => {
-  return <h1>This is the article page!</h1>
+  const { articleId } = useParams()
+  const article = articles.find((article) => article.name === articleId)
+
+  return (
+    <>
+      <h1>{article.title}</h1>
+      {article.content.map((paragraph, i) => (
+        <p key={i}>{paragraph}</p>
+      ))}
+    </>
+  )
 }
 
 export default ArticlePage
