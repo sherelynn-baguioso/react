259e1ce25c77b0a45053a677487f649c3a51316d
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-01-27 14:41:22 +1300

    Add comments

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -4,14 +4,17 @@ let articlesinfo = [
   {
     name: 'learn-react',
     upvotes: 0,
+    comments: [],
   },
   {
     name: 'learn-node',
     upvotes: 0,
+    comments: [],
   },
   {
     name: 'mongodb',
     upvotes: 0,
+    comments: [],
   },
 ]
 
@@ -28,6 +31,18 @@ app.put('/api/articles/:name/upvote', (req, res) => {
     res.send(`The ${name} article now has ${article.upvotes} upvotes!!!`)
   } else {
     res.send("That article doesn't exist")
+  }
+})
+
+app.post('/api/articles/:name/comments', (req, res) => {
+  const { name } = req.params
+  const { postedBy, text } = req.body
+  const article = articlesinfo.find((a) => a.name === name)
+  if (article) {
+    article.comments.push({ postedBy, text })
+    res.send(article.comments)
+  } else {
+    res.send("That article doesn't exist!")
   }
 })
 
