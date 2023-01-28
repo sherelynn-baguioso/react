02324c502bd72c616285bf8d60f8f13abf656402
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-01-29 02:00:06 +1300

    Create a functional upvote button

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -16,9 +16,15 @@ const ArticlePage = () => {
       setArticleInfo(newArticleInfo)
     }
     loadArticleInfo()
-  })
+  }, [])
 
   const article = articles.find((article) => article.name === articleId)
+
+  const addUpvote = async () => {
+    const response = await axios.put(`/api/articles/${articleId}/upvote`)
+    const updatedArticle = response.data
+    setArticleInfo(updatedArticle)
+  }
 
   if (!article) {
     return <NotFoundPage />
@@ -27,7 +33,10 @@ const ArticlePage = () => {
   return (
     <>
       <h1>{article.title}</h1>
-      <p>This article has {articleInfo.upvotes} upvote(s).</p>
+      <div className="upvotes-section">
+        <button onClick={addUpvote}>Upvote</button>
+        <p>This article has {articleInfo.upvotes} upvote(s).</p>
+      </div>
       {article.content.map((paragraph, i) => (
         <p key={i}>{paragraph}</p>
       ))}
