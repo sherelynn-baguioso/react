a8ad5e51f551bdb0a111269f519fb5f2ca1e3644
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-01-31 08:52:15 +1300

    Protect upvote and comment endpoints

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -41,20 +41,36 @@ app.get('/api/articles/:name', async (req, res) => {
   }
 })
 
+app.use((req, res, next) => {
+  if (req.user) {
+    next()
+  } else {
+    res.sendStatus(401)
+  }
+})
+
 app.put('/api/articles/:name/upvote', async (req, res) => {
   const { name } = req.params
-
-  await db.collection('articles').updateOne(
-    { name },
-    {
-      $inc: { upvotes: 1 },
-    }
-  )
+  const { uid } = req.user
 
   const article = await db.collection('articles').findOne({ name })
 
   if (article) {
-    res.json(article)
+    const upvoteIds = article.upvoteIds || []
+    const canUpvote = uid && !upvoteIds.include(uid)
+
+    if (canUpvote) {
+      await db.collection('articles').updateOne(
+        { name },
+        {
+          $inc: { upvotes: 1 },
+          $push: { upvoteIds: uid },
+        }
+      )
+    }
+
+    const updatedArticle = await db.collection('articles').findOne({ name })
+    res.json(updatedArticle)
   } else {
     res.send("That article doesn't exist")
   }
@@ -62,12 +78,13 @@ app.put('/api/articles/:name/upvote', async (req, res) => {
 
 app.post('/api/articles/:name/comments', async (req, res) => {
   const { name } = req.params
-  const { postedBy, text } = req.body
+  const { text } = req.body
+  const { email } = req.user
 
   await db.collection('articles').updateOne(
     { name },
     {
-      $push: { comments: { postedBy, text } },
+      $push: { comments: { postedBy: email, text } },
     }
   )
 
