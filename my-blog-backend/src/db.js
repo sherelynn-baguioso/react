61901fbec2cc42dc364bc760d150318ddc6cf4eb
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-01-31 11:36:49 +1300

    Set up hosting for MongoDB

--- my-blog-backend/src/db.js
+++ my-blog-backend/src/db.js
@@ -3,7 +3,9 @@ import { MongoClient } from 'mongodb'
 let db
 
 async function connectToDb(cb) {
-  const client = new MongoClient('mongodb://127.0.0.1:27017')
+  const client = new MongoClient(
+    `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.rr1v7na.mongodb.net/?retryWrites=true&w=majority`
+  )
   await client.connect()
   db = client.db('react-blog-db')
   cb()
