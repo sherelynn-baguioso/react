bc9d1672ac1543372ee4517a2f729eeacc7b99eb
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-01-30 13:38:46 +1300

    Create a custom auth hook

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -1,14 +1,17 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import axios from 'axios'
-import articles from './article-content'
+import NotFoundPage from './NotFoundPage'
 import CommentsList from '../components/CommentsList'
 import AddCommentForm from '../components/AddCommentForm'
-import NotFoundPage from './NotFoundPage'
+import useUser from '../hooks/useUser'
+import articles from './article-content'
 
 const ArticlePage = () => {
   const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] })
   const { articleId } = useParams()
+
+  const { user, isLoading } = useUser()
 
   useEffect(() => {
     const loadArticleInfo = async () => {
@@ -35,15 +38,24 @@ const ArticlePage = () => {
     <>
       <h1>{article.title}</h1>
       <div className="upvotes-section">
-        <button onClick={addUpvote}>Upvote</button>
+        {user ? (
+          <button onClick={addUpvote}>Upvote</button>
+        ) : (
+          <button>Log in to upvote</button>
+        )}
         <p>This article has {articleInfo.upvotes} upvote(s).</p>
       </div>
       {article.content.map((paragraph, i) => (
         <p key={i}>{paragraph}</p>
       ))}
-      <AddCommentForm 
-        articleName={articleId}
-        onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
+      {user ? (
+        <AddCommentForm
+          articleName={articleId}
+          onArticleUpdated={(updatedArticle) => setArticleInfo(updatedArticle)}
+        />
+      ) : (
+        <button>Log in to add a comment</button>
+      )}
       <CommentsList comments={articleInfo.comments} />
     </>
   )
