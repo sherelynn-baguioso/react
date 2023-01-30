0261c51b22ef1516387e79a2b7a43b51a72c3164
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-01-31 09:27:36 +1300

    Make interface adjustments for authenticated users

--- my-blog/src/pages/ArticlePage.js
+++ my-blog/src/pages/ArticlePage.js
@@ -8,24 +8,42 @@ import useUser from '../hooks/useUser'
 import articles from './article-content'
 
 const ArticlePage = () => {
-  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] })
+  const [articleInfo, setArticleInfo] = useState({
+    upvotes: 0,
+    comments: [],
+    canUpvote: false,
+  })
+  const { canUpvote } = articleInfo
   const { articleId } = useParams()
 
   const { user, isLoading } = useUser()
 
   useEffect(() => {
     const loadArticleInfo = async () => {
-      const response = await axios.get(`/api/articles/${articleId}`)
+      const token = user && (await user.getIdToken())
+      const headers = token ? { authToken: token } : {}
+      const response = await axios.get(`/api/articles/${articleId}`, {
+        headers: { authToken: token },
+      })
       const newArticleInfo = response.data
       setArticleInfo(newArticleInfo)
     }
-    loadArticleInfo()
-  }, [])
+
+    if (isLoading) {
+      loadArticleInfo()
+    }
+  }, [isLoading, user])
 
   const article = articles.find((article) => article.name === articleId)
 
   const addUpvote = async () => {
-    const response = await axios.put(`/api/articles/${articleId}/upvote`)
+    const token = user && (await user.getIdToken())
+    const headers = token ? { authToken: token } : {}
+    const response = await axios.put(
+      `/api/articles/${articleId}/upvote`,
+      null,
+      { headers }
+    )
     const updatedArticle = response.data
     setArticleInfo(updatedArticle)
   }
@@ -39,7 +57,9 @@ const ArticlePage = () => {
       <h1>{article.title}</h1>
       <div className="upvotes-section">
         {user ? (
-          <button onClick={addUpvote}>Upvote</button>
+          <button onClick={addUpvote}>
+            {canUpvote ? 'Upvote' : 'Already Upvoted'}
+          </button>
         ) : (
           <button>Log in to upvote</button>
         )}
