4b5c313e935a6cf9b68a5a203bb4f294b460967e
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-01-31 08:41:57 +1300

    Protect endpoints using auth-tokens

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -12,12 +12,29 @@ const app = express()
 
 app.use(express.json())
 
+app.use(async (req, res, next) => {
+  const { authtoken } = req.headers
+
+  if (authtoken) {
+    try {
+      req.user = await admin.auth().verifyIdToken(authtoken)
+    } catch (e) {
+      res.sendStatus(400)
+    }
+  }
+
+  next()
+})
+
 app.get('/api/articles/:name', async (req, res) => {
   const { name } = req.params
+  const { uid } = req.user
 
   const article = await db.collection('articles').findOne({ name })
 
   if (article) {
+    const upvoteIds = article.upvoteIds || []
+    article.canUpvote = uid && !upvoteIds.include(uid)
     res.json(article)
   } else {
     res.sendStatus(404)
