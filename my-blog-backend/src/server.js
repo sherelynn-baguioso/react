5064e6e7a4937410f84664d7d05fb9069b8462e7
sherelynn-baguioso <107873828+sherelynn-baguioso@users.noreply.github.com>
2023-01-31 09:23:45 +1300

    Add return for catch; Add req.user; Fix include/includes

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -3,7 +3,7 @@ import admin from 'firebase-admin'
 import express from 'express'
 import { db, connectToDb } from './db.js'
 
-const credentials = JSON.parse(fs.readFileSync('../credentials.json'))
+const credentials = JSON.parse(fs.readFileSync('./credentials.json'))
 admin.initializeApp({
   credential: admin.credential.cert(credentials),
 })
@@ -19,9 +19,11 @@ app.use(async (req, res, next) => {
     try {
       req.user = await admin.auth().verifyIdToken(authtoken)
     } catch (e) {
-      res.sendStatus(400)
+      return res.sendStatus(400)
     }
   }
+
+  req.user = req.user || {}
 
   next()
 })
@@ -34,7 +36,7 @@ app.get('/api/articles/:name', async (req, res) => {
 
   if (article) {
     const upvoteIds = article.upvoteIds || []
-    article.canUpvote = uid && !upvoteIds.include(uid)
+    article.canUpvote = uid && !upvoteIds.includes(uid)
     res.json(article)
   } else {
     res.sendStatus(404)
@@ -57,7 +59,7 @@ app.put('/api/articles/:name/upvote', async (req, res) => {
 
   if (article) {
     const upvoteIds = article.upvoteIds || []
-    const canUpvote = uid && !upvoteIds.include(uid)
+    const canUpvote = uid && !upvoteIds.includes(uid)
 
     if (canUpvote) {
       await db.collection('articles').updateOne(
